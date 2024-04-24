446f534ece3970356f8b74297ac1412364d32fb6
santigm12 <santiago.sgm12@gmail.com>
2024-04-24 09:58:08 +0200

    modificacion de la interfaz Pincel

--- Paint/src/main/java/clases/Pincel.java
+++ Paint/src/main/java/clases/Pincel.java
@@ -1,7 +1,8 @@
 package clases;
 
+import javafx.scene.canvas.GraphicsContext;
+
 public interface Pincel {
-    void dibujar(GraphicsContext g, Punto p){
-        throw new UnsupportedOperationException("Sin programar");
-    }
+    void dibujar(GraphicsContext g, Punto p);
+
 }
--- Paint/src/main/java/test/PincelTester.java
+++ Paint/src/main/java/test/PincelTester.java
@@ -1,7 +1,9 @@
 package test;
 
+import clases.TipoPincel;
+
 public class PincelTester {
-    public static final TipoPincel TIPO_PINCEL;
+    public static final TipoPincel TIPO_PINCEL = null;
 
     public static void main(String [] args){
         throw new UnsupportedOperationException("Sin programar");
