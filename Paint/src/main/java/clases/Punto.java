84dd7c157f3fd9c154bd5696d42ffec5b02c2925
rmornun12 <roberto.m.n2005@gmail.com>
2024-04-24 09:55:05 +0200

    Commit punto_actualizado

--- Paint/src/main/java/clases/Punto.java
+++ Paint/src/main/java/clases/Punto.java
@@ -1,4 +1,9 @@
 package clases;
 
 public record Punto(double x, double y) {
+    public Punto{
+        if(this.x()<0 && this.y()<0){
+            throw new IllegalArgumentException("x e y son menores que 0.");
+        }
+    }
 }
