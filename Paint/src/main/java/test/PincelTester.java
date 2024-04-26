22da43fdf37c2c8d5f3db5a612fea9c3ceabb1ee
santigm12 <santiago.sgm12@gmail.com>
2024-04-26 09:09:05 +0200

    LineaDiagonal, PincelTester programados

--- Paint/src/main/java/test/PincelTester.java
+++ Paint/src/main/java/test/PincelTester.java
@@ -1,15 +1,36 @@
 package test;
 
 import clases.TipoPincel;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
-public class PincelTester {
+public class PincelTester extends Application {
+
+    /*PARA COMPROBAR LOS PINCELES SE TIENE QUE CAMBIAR
+      EL NULL POR EL TIPO DE PINCEL QUE SE QUIERE COMPROBAR*/
     public static final TipoPincel TIPO_PINCEL = null;
 
-    public static void main(String [] args){
-        throw new UnsupportedOperationException("Sin programar");
+    public static void main(String[] args) {
+        launch(args);
     }
 
-    public void start() throws Exception{
-        throw new UnsupportedOperationException("Sin programar");
+    @Override
+    public void start(Stage primaryStage) {
+        Canvas c = new Canvas(400,400);
+        StackPane sp = new StackPane(c);
+        Scene s = new Scene(sp);
+        GraphicsContext gc = c.getGraphicsContext2D();
+        gc.setFill(Color.BLACK);
+        primaryStage.setScene(s);
+        primaryStage.show();
+
+        //COMPROBAR SI ESTA BIEN
+        LineaDiagonal ld = new LineaDiagonal(TIPO_PINCEL.getPincel());
+        ld.dibujarLinea(gc);
     }
 }
