3625e16b072fe36de607b072f31d8047db5f1c2a
christdc <cristinadiazcabello@gmail.com>
2024-04-26 09:58:41 +0200

    Hecho scene builder

--- Paint/src/main/java/controlador/ControladorfrmPaint.java
+++ Paint/src/main/java/controlador/ControladorfrmPaint.java
@@ -1,4 +1,60 @@
 package controlador;
 
+import clases.Pincel;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ComboBox;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+
 public class ControladorfrmPaint {
+
+    @FXML
+    private Button btnAbrir;
+
+    @FXML
+    private Button btnGuardar;
+
+    @FXML
+    private Button btnNuevo;
+
+    @FXML
+    private ComboBox<Pincel> cmbPinceles;
+
+    @FXML
+    private Canvas cnvLienzo;
+
+    @FXML
+    private ColorPicker cpkColor;
+
+    @FXML
+    private ImageView imgPinceles;
+
+    @FXML
+    private BorderPane panel;
+
+    @FXML
+    void abrirImagen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void dibujarPunto(MouseEvent event) {
+
+    }
+
+    @FXML
+    void empezarDibujar(MouseEvent event) {
+
+    }
+
+    @FXML
+    void guardarImagen(ActionEvent event) {
+
+    }
+
 }
