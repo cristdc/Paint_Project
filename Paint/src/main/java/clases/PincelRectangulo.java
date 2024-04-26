7e117c3e36f09767ea8468e3c8a14229bdae7229
paablogarciaa <pablogarciaborjaa@gmail.com>
2024-04-26 09:25:12 +0200

    programacion pincel estrella y PincelRectangulo

--- Paint/src/main/java/clases/PincelRectangulo.java
+++ Paint/src/main/java/clases/PincelRectangulo.java
@@ -1,8 +1,20 @@
 package clases;
 
 
-public class PincelRectangulo {
+import javafx.scene.canvas.GraphicsContext;
+
+public class PincelRectangulo implements Pincel {
     PincelRectangulo(){
-        throw new UnsupportedOperationException();
+
+    }
+
+    @Override
+    public void dibujar(GraphicsContext g, Punto p) {
+        g.fillRect(p.x(),p.y(),10,10);
+    }
+
+    @Override
+    public String toString() {
+        return "Pincel rectangular";
     }
 }
